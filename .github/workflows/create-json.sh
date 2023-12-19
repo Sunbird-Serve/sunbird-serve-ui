24b821ec64d0c6ea6c98e7b0463a33b6224babf9
Sowmya-Raghuram <sowmya.raghuram@evidyaloka.org>
2023-12-19 17:15:20 +0530

    CI/CD setup

--- .github/workflows/create-json.sh
+++ .github/workflows/create-json.sh
@@ -4,10 +4,21 @@ if [ -f /tmp/__secrets.json ] ; then
 rm -f /tmp/__secrets.json 
 fi
 
+branch_name=${GITHUB_REF#refs/heads/}
+echo $branch_name
+
+if [ "$branch_name" == "main" ] ; then
+prefix=stage
+#region="ap-south-1"
+elif [ "$branch_name" == release-* ] ; then
+prefix=prod
+#region=us-east-1
+fi
 printf '{
 	 "secrets": [' > /tmp/__secrets.json
 
-for line in `cat .env`
+
+for line in `cat .${prefix}env`
 do
   key=$line
   if [[ -z "$line" || "$line" =~ ^[[:space:]]*# ]]; then
@@ -20,6 +31,6 @@ do
 done
 sed '$ s/,$//'  /tmp/__secrets.json > .github/workflows/secrets.json
 
-echo ']}' >> .github/workflows/secrets.json 
+echo ']}' >> .github/workflows/secrets.json
 
 cat .github/workflows/secrets.json
--- .github/workflows/replace-secrets.py
+++ .github/workflows/replace-secrets.py
@@ -5,37 +5,36 @@ secrets_array = ".github/workflows/secrets.json"
 
 
 with open(original_taskdef, 'r') as file:
-	task_definition = json.load(file)
+    task_definition = json.load(file)
 
-	for item in task_definition:
-		if "containerDefinitions" in item:
-			containerDefinitions = task_definition['containerDefinitions']
-			for item in containerDefinitions:
-				if "secrets" in item:
-					print("secret block exists")
-					#print(json.dumps(containerDefinitions[0]['secrets'], indent=4)) # if want to print
-			
-			
+    for item in task_definition:
+        if "containerDefinitions" in item:
+            containerDefinitions = task_definition['containerDefinitions']
+            for item in containerDefinitions:
+                if "secrets" in item:
+                    print("secret block exists")
+                    # print(json.dumps(containerDefinitions[0]['secrets'], indent=4)) # if want to print
+
 
 with open(secrets_array, 'r') as new_file:
-	new_data = json.load(new_file)
-	new_secrets	= new_data['secrets']
-	#print(json.dumps(new_secrets, indent=4))
+    new_data = json.load(new_file)
+    new_secrets = new_data['secrets']
+    # print(json.dumps(new_secrets, indent=4))
 
-#__task_definition = task_definition["taskDefinition"]
-#__secrets_to_replace = __task_definition["containerDefinitions"] 
-#secrets_to_replace = __secrets_to_replace[0]["secrets"]
-#__secrets_to_replace[0]["secrets"] = new_secrets
+# __task_definition = task_definition["taskDefinition"]
+# __secrets_to_replace = __task_definition["containerDefinitions"]
+# secrets_to_replace = __secrets_to_replace[0]["secrets"]
+# __secrets_to_replace[0]["secrets"] = new_secrets
 
 
-__secrets_to_replace = task_definition['containerDefinitions'] 
+__secrets_to_replace = task_definition['containerDefinitions']
 secrets_to_replace = __secrets_to_replace[0]['secrets']
 __secrets_to_replace[0]['secrets'] = new_secrets
 
-#task_definition['secrets'] = new_secrets		#replace old secrets block with new_secrets
+# task_definition['secrets'] = new_secrets		#replace old secrets block with new_secrets
 
 
-print(json.dumps(task_definition, indent = 4))
+print(json.dumps(task_definition, indent=4))
 
 
 # writing to new file
