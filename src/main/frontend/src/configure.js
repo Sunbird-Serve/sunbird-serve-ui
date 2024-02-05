60967ee6089c7520ed642f869598ba9659de546e
Sowmya-Raghuram <sowmya.raghuram@evidyaloka.org>
2024-02-05 14:25:33 +0530

    Updated workflow for ekal and conf file

--- src/main/frontend/src/configure.js
+++ src/main/frontend/src/configure.js
@@ -1,8 +1,8 @@
 //const baseDomain = "https://serve-v1.evean.net"; //https://ekal.evean.net
 const baseDomain = "https://serve-v1.evean.net";
-const baseDomainNeed = "https://serve-v1.evean.net"
-const baseDomainFulfill = "https://serve-v1.evean.net"
-const baseDomainVolunteering = "https://serve-v1.evean.net"
+const baseDomainNeed = "https://ekal.evean.net"
+const baseDomainFulfill = "https://ekal.evean.net"
+const baseDomainVolunteering = "https://ekal.evean.net"
 
 const configData = {
     "USER_GET" : `${baseDomainVolunteering}/api/v1/serve-volunteering/user`,
@@ -10,7 +10,7 @@ const configData = {
     "NEED_SEARCH": `${baseDomainFulfill}/api/v1/serve-fulfill/nomination`,
     "NEED_FULFILL":`${baseDomainFulfill}/api/v1/serve-fulfill/nomination`,
     "NEEDTYPE_GET": `${baseDomainNeed}/api/v1/serve-need/needtype`,
-    "ENTITY_GET": `https://serve-v1.evean.net/api/v1/entity`,
+    "ENTITY_GET": `https://ekal.evean.net/api/v1/entity`,
     "NEED_POST" : `${baseDomainNeed}/api/v1/serve-need/need/raise`,
     "NEED_BY_TYPE" : `${baseDomainNeed}/api/v1/need/serve-need/need-type/create`,
     "NOMINATED_USER_FETCH" : `${baseDomainVolunteering}/api/v1/serve-volunteering/user`,
