2d302d22baccd9e3cb52b74a18b7dbf4acac469c
anshul-251204 <annshulch1@gmail.com>
2024-11-28 14:40:19 +0530

    direct redirection to online teaching page

--- src/main/frontend/src/state/store.js
+++ src/main/frontend/src/state/store.js
@@ -1,20 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './userSlice'
-import needReducer from './needSlice'
-import userListReducer from './userListSlice'
-import needtyperReducer from './needtypeSlice'
-import entityReducer from './entitySlice'
-import needByUidReducer from './needByUidSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./userSlice";
+import needReducer from "./needSlice";
+import userListReducer from "./userListSlice";
+import needtyperReducer from "./needtypeSlice";
+import entityReducer from "./entitySlice";
+import needByUidReducer from "./needByUidSlice";
+import redirectToOnlineTeachingReducer from "./redirectionToOnlineTeachingSlice";
 
 const store = configureStore({
-    reducer: {
-        need: needReducer,
-        user: userReducer,
-        userlist: userListReducer,
-        needtype: needtyperReducer,
-        entity: entityReducer,
-        needbyuid: needByUidReducer,
-    },
-})
+	reducer: {
+		need: needReducer,
+		user: userReducer,
+		userlist: userListReducer,
+		needtype: needtyperReducer,
+		entity: entityReducer,
+		needbyuid: needByUidReducer,
+		redirectToOnlineTeaching: redirectToOnlineTeachingReducer,
+	},
+});
 
 export default store;
